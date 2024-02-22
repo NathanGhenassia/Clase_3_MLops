91af2d718f6d191da15ffb9ea61589c5cd7941e4
NathanGhenassia <nathan.ghenassia@gmail.com>
2024-02-22 16:45:28 -0600

    Funcion resta

--- main.py
+++ main.py
@@ -2,10 +2,20 @@
 def suma(num1: int, num2: int) -> int:
     return num1 + num2
 
+
 if __name__ == '__main__':
     print(suma(num1=2, num2=4))
 
 
 
-#Resta
+#Codigo para la resta de dos numeros
+def resta(num1: int, num2: int) -> int:
+    return num1 - num2
+
+
+if __name__ == '__main__':
+    print(resta(num1=2, num2=5))
+
+
+
 #Reconocimieto de gatos y perros
