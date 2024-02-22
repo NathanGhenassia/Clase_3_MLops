b8968e70a931cc86150db4feb888b6a80a323239
NathanGhenassia <nathan.ghenassia@gmail.com>
2024-02-22 16:41:10 -0600

    Funcion suma

---
+++ main.py
@@ -0,0 +1,11 @@
+#Codigo para la suma de dos numeros
+def suma(num1: int, num2: int) -> int:
+    return num1 + num2
+
+if __name__ == '__main__':
+    print(suma(num1=2, num2=4))
+
+
+
+#Resta
+#Reconocimieto de gatos y perros
